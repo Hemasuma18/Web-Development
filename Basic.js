f62bbcbf345c319294099b4cbc13336e477ecdda
Hemasuma18 <hemasumagurajarla@gmail.com>
2025-05-28 19:30:44 +0530

    Add files via upload

---
+++ Basic.js
@@ -0,0 +1,9 @@
+
+       
+           function showImage(){
+            document.getElementById("imageContainer").style.display="block";
+           }
+            function showFact(){
+            document.getElementById("fact").style.display="block";
+            }
+          
